42bd9268afafdb50d43c9e3698347d74946bf864
Kato <45398208+AK56xXx@users.noreply.github.com>
2023-11-23 13:09:00 +0100

    v0

---
+++ admin/src/components/navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
+
+const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon
+              className="icon"
+              onClick={() => dispatch({ type: "TOGGLE" })}
+            />
+          </div>
+          
+          <div className="item">
+            Ahmed
+            <img
+              src="https://icons8.com/icon/kDoeg22e5jUY/male-user"
+              alt=""
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
